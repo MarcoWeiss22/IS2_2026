eed2a38b9df60c86dbdfb206577f21d52e73983a
MarcoWeiss22 <marcoweiss22@gmail.com>
2026-03-25 21:17:59 -0300

    Agrego comentarios a primos.py

--- src/primos.py
+++ src/primos.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-# Python program to display all the prime numbers within an interval
 
-lower = 1
-upper = 500
+# Programa que muestra números primos en un intervalo
+
+lower = 1   # límite inferior
+upper = 500 # límite superior
 
 print("Prime numbers between", lower, "and", upper, "are:")
 
+# recorrer todos los números del intervalo
 for num in range(lower, upper + 1):
-   # all prime numbers are greater than 1
+
+   # los números primos son mayores que 1
    if num > 1:
+
+       # verificar si tiene divisores
        for i in range(2, num):
            if (num % i) == 0:
                break
